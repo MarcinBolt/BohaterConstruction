632cb42a9f127077518fa2a78c4ba5185ea76768
MarcinBolt <marcin.boltruczyk@gmail.com>
2024-06-13 00:45:54 +0200

    change images src jpg to jpeg in HomeView

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -47,21 +47,21 @@ const services = [
   {
     id: 1,
     title: 'Residential Construction',
-    imageSrc: './src/assets/images/ResidentialConstruction.jpg',
+    imageSrc: './src/assets/images/ResidentialConstruction.jpeg',
     description:
       'From custom homes to renovations, I specialize in bringing your residential dreams to life with meticulous attention to detail and exceptional craftsmanship.',
   },
   {
     id: 2,
     title: 'Commercial Construction',
-    imageSrc: './src/assets/images/CommercialConstruction.jpg',
+    imageSrc: './src/assets/images/CommercialConstruction.jpeg',
     description:
       'I offer comprehensive commercial construction services tailored to meet the unique needs of your business, ensuring a seamless and efficient project execution.',
   },
   {
     id: 3,
     title: 'Sustainable Construction',
-    imageSrc: './src/assets/images/SustainableConstruction.jpg',
+    imageSrc: './src/assets/images/SustainableConstruction.jpeg',
     description:
       'My eco-friendly approach ensures that your project is not only aesthetically pleasing but also environmentally conscious, incorporating sustainable practices and materials for a greener future.',
   },
