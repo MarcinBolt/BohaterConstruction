63bd0de01207b30f05ef1c656fa346c6af9f09ed
MarcinBolt <marcin.boltruczyk@gmail.com>
2024-06-13 00:05:00 +0200

    install normalize.css and import in index.html, add a new animation in HomeView.vue for services, add a new images for services

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -8,23 +8,34 @@
       exceptional craftsmanship and unparalleled service.
     </p>
     <ButtonWithTransition text="Realized projects" :to="{ name: 'projects' }" />
-    <section class="services">
+    <div class="container">
       <div
         v-for="(service, index) in services"
         :key="index"
-        class="service-item"
+        class="card"
         :tabindex="0"
-        :class="{ 'service-item--focus': isServiceItemFocused === index }"
-        @focus="onServiceItemFocus(index)"
-        @blur="onServiceItemBlur"
+        :class="{ 'card--focus': isCardFocused === index }"
+        @focus="onCardFocus(index)"
+        @blur="onCardBlur"
       >
-        <h3 class="service-item-title">{{ service.title }}</h3>
-        <p class="service-item-description">{{ service.description }}</p>
-        <router-link class="service-item-link" :to="`/services/${service.id}`"
-          >Learn More</router-link
-        >
+        <img class="background" :src="service.imageSrc" alt="" />
+        <div class="card-content">
+          <div class="service-icon">
+            <svg xmlns="http://www.w3.org/2000/svg" id="icon-eye" viewBox="0 0 32 32" fill="none">
+              <path
+                d="M16 6c-6.979 0-13.028 4.064-16 10 2.972 5.936 9.021 10 16 10s13.027-4.064 16-10c-2.972-5.936-9.021-10-16-10zM23.889 11.303c1.88 1.199 3.473 2.805 4.67 4.697-1.197 1.891-2.79 3.498-4.67 4.697-2.362 1.507-5.090 2.303-7.889 2.303s-5.527-0.796-7.889-2.303c-1.88-1.199-3.473-2.805-4.67-4.697 1.197-1.891 2.79-3.498 4.67-4.697 0.122-0.078 0.246-0.154 0.371-0.228-0.311 0.854-0.482 1.776-0.482 2.737 0 4.418 3.582 8 8 8s8-3.582 8-8c0-0.962-0.17-1.883-0.482-2.737 0.124 0.074 0.248 0.15 0.371 0.228v0zM16 13c0 1.657-1.343 3-3 3s-3-1.343-3-3 1.343-3 3-3 3 1.343 3 3z"
+              ></path>
+            </svg>
+          </div>
+          <div class="service-content">
+            <p class="description">{{ service.description }}</p>
+            <router-link class="link" :to="`/services/${service.id}`">Learn More</router-link>
+            <h3 class="title">{{ service.title }}</h3>
+          </div>
+        </div>
+        <div class="backdrop"></div>
       </div>
-    </section>
+    </div>
   </main>
 </template>
 
@@ -36,31 +47,34 @@ const services = [
   {
     id: 1,
     title: 'Residential Construction',
+    imageSrc: './src/assets/images/ResidentialConstruction.png',
     description:
       'From custom homes to renovations, I specialize in bringing your residential dreams to life with meticulous attention to detail and exceptional craftsmanship.',
   },
   {
     id: 2,
     title: 'Commercial Construction',
+    imageSrc: './src/assets/images/CommercialConstruction.png',
     description:
       'I offer comprehensive commercial construction services tailored to meet the unique needs of your business, ensuring a seamless and efficient project execution.',
   },
   {
     id: 3,
     title: 'Sustainable Construction',
+    imageSrc: './src/assets/images/SustainableConstruction.png',
     description:
       'My eco-friendly approach ensures that your project is not only aesthetically pleasing but also environmentally conscious, incorporating sustainable practices and materials for a greener future.',
   },
 ];
 
-const isServiceItemFocused = ref(null);
+const isCardFocused = ref(null);
 
-const onServiceItemFocus = index => {
-  isServiceItemFocused.value = index;
+const onCardFocus = index => {
+  isCardFocused.value = index;
 };
 
-const onServiceItemBlur = () => {
-  isServiceItemFocused.value = null;
+const onCardBlur = () => {
+  isCardFocused.value = null;
 };
 </script>
 
@@ -80,53 +94,139 @@ const onServiceItemBlur = () => {
   }
 }
 
-.services {
-  display: flex;
-  flex-wrap: wrap;
-  gap: 20px;
-  justify-content: center;
-  margin-top: 50px;
-}
-
-.service-item {
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  align-items: center;
-  width: 300px;
-  padding: 30px;
+*::before,
+*::after {
   margin: 0;
-  background-color: rgba(0, 0, 0, 0.3);
+  padding: 0;
+  box-sizing: border-box;
+}
+mg {
+  width: 100%;
+  height: 100%;
+  display: block;
+}
+
+.container {
+  width: 100%;
+  max-width: 900px;
+  height: 400px;
+  display: flex;
+  gap: 2px;
+  justify-content: center;
+  padding: 50px 0 70px 0;
+}
+
+.card {
+  min-width: 60px;
+  height: 100%;
+  border: 1px dashed #a128fe;
+  border-radius: 15px;
+  overflow: hidden;
+  display: flex;
+  align-items: flex-end;
+  flex-grow: 1;
+  position: relative;
+  transition: flex-grow 0.3s ease-in;
+  padding: 0;
+  --transition-timing: 0.5s;
+}
+.card:hover,
+.card--focus {
+  flex-grow: 20;
+  border: 1px solid #dcb4fb;
+}
+
+.card > .background {
+  position: absolute;
+  inset: 0;
+  object-fit: cover;
+  object-position: center;
+  filter: brightness(0.4);
+  z-index: -1;
+  transition: var(--transition-timing) ease;
+}
+.card:hover > .background,
+.card:focus-within > .background {
+  filter: brightness(1);
+}
+
+.card > .card-content {
+  display: flex;
+  align-items: end;
+  position: absolute;
+  left: 12px;
+  bottom: 5px;
+  overflow: visible;
+  transition: var(--transition-timing);
+  z-index: 10;
+}
+.card:hover > .card-content,
+.card:focus-within > .card-content {
+  inset: 20px;
+  top: auto;
+}
+
+.card-content > * {
+  transition: var(--transition-timing);
+}
+
+.card-content > .service-icon {
+  min-width: 35px;
+  max-width: 35px;
+  height: 35px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 0;
+  overflow: hidden;
+}
+.card:hover .service-icon,
+.card:focus .service-icon {
+  display: none;
+}
+.service-icon > svg {
+  stroke: #dcb4fb;
+}
+.card:hover .service-icon > svg,
+.card:focus .service-icon > svg {
+  display: none;
+}
+
+.service-content {
+  display: flex;
+  gap: 20px;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  -webkit-box-shadow: 5px 5px 18px 2px rgba(0, 0, 0, 1);
+  -moz-box-shadow: 5px 5px 18px 2px rgba(0, 0, 0, 1);
+  box-shadow: 5px 5px 18px 2px rgba(0, 0, 0, 1);
+  margin: 2 auto;
+  padding: 10px;
+  opacity: 0;
+  translate: 0 100%;
+  text-align: left;
+  transition: opacity 0s, translate 1.5s ease;
+}
+.card:hover .service-content,
+.card:focus .service-content {
   border-radius: 10px;
-  text-align: center;
-  transition: all 0.3s ease;
+  background-color: rgba(31, 6, 51, 0.65);
+  opacity: 1;
+  translate: 0 0;
 }
 
-.service-item--focus,
-.service-item--hover {
-  transform: translateY(-5px);
-  box-shadow: 0 8px 12px rgba(0, 0, 0, 0.499);
+.card > .backdrop {
+  position: absolute;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  height: 120px;
+  z-index: 0;
+  background: linear-gradient(to bottom, transparent, rgb(0, 0, 0));
 }
-
-.service-item-title {
-  font-size: 24px;
-  margin-bottom: 20px;
-}
-
-.service-item-description {
-  font-size: 18px;
-  margin-bottom: 30px;
-}
-
-.service-item-link {
-  color: #00c3ff;
-  text-decoration: none;
-  font-weight: bold;
-  transition: color 0.3s ease;
-}
-
-.service-item .service-item-link:hover,
-.service-item .service-item-link:focus {
-  color: #dcb4fb;
+.card:hover .backdrop,
+.card:focus .backdrop {
+  background: transparent;
 }
 </style>
