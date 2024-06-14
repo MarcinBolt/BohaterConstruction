5a13b8d713e506b277d50c71db51248fce91640b
MarcinBolt <marcin.boltruczyk@gmail.com>
2024-06-14 20:37:12 +0200

    fix displaying svg icon (eye) on safari

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -21,7 +21,14 @@
         <img class="background" :src="service.imageSrc" :alt="service.title" />
         <div class="card-content">
           <div class="service-icon">
-            <svg xmlns="http://www.w3.org/2000/svg" id="icon-eye" viewBox="0 0 32 32" fill="none">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              id="icon-eye"
+              viewBox="0 0 34 26"
+              width="34"
+              height="26"
+              fill="none"
+            >
               <path
                 d="M16 6c-6.979 0-13.028 4.064-16 10 2.972 5.936 9.021 10 16 10s13.027-4.064 16-10c-2.972-5.936-9.021-10-16-10zM23.889 11.303c1.88 1.199 3.473 2.805 4.67 4.697-1.197 1.891-2.79 3.498-4.67 4.697-2.362 1.507-5.090 2.303-7.889 2.303s-5.527-0.796-7.889-2.303c-1.88-1.199-3.473-2.805-4.67-4.697 1.197-1.891 2.79-3.498 4.67-4.697 0.122-0.078 0.246-0.154 0.371-0.228-0.311 0.854-0.482 1.776-0.482 2.737 0 4.418 3.582 8 8 8s8-3.582 8-8c0-0.962-0.17-1.883-0.482-2.737 0.124 0.074 0.248 0.15 0.371 0.228v0zM16 13c0 1.657-1.343 3-3 3s-3-1.343-3-3 1.343-3 3-3 3 1.343 3 3z"
               ></path>
@@ -121,7 +128,7 @@ mg {
 }
 
 .card {
-  min-width: 50px;
+  min-width: 55px;
   height: 100%;
   border: 1px dashed #a128fe;
   border-radius: 15px;
@@ -175,21 +182,14 @@ mg {
 }
 
 .card-content > .service-icon {
-  min-width: 25px;
-  max-width: 25px;
-  height: 25px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   padding: 0;
-  overflow: hidden;
 }
 .card:hover .service-icon,
 .card:focus .service-icon {
   display: none;
 }
 .service-icon > svg {
-  stroke: #dcb4fb;
+  fill: #dcb4fb;
 }
 .card:hover .service-icon > svg,
 .card:focus .service-icon > svg {
