d146a11d1c626b8e7c21ebb4aca9a584ddfa4888
MarcinBolt <marcin.boltruczyk@gmail.com>
2024-06-20 19:15:33 +0200

    add condition for show arrows in v-carousel - now arrows are available on hover only for screens min-width = 1024px

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -2,7 +2,7 @@
   <main>
     <v-container class="container h-full">
       <h1 class="heading">Completed Projects</h1>
-      <v-carousel show-arrows="hover" cycle hide-delimiter-background>
+      <v-carousel :show-arrows="showArrows" cycle interval="8000" hide-delimiter-background>
         <v-carousel-item v-for="project in projects" :key="`carousel-${project.id}`">
           <ProjectCard :key="project.id" :project="project" />
         </v-carousel-item>
@@ -12,11 +12,26 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed, onMounted, onUnmounted } from 'vue';
 import ProjectCard from '@/components/ProjectCard.vue';
 import projectsData from '@/data/projects.json';
 
 const projects = ref(projectsData);
+const windowWidth = ref(window.innerWidth);
+
+const showArrows = computed(() => (windowWidth.value >= 1024 ? 'hover' : false));
+
+const updateWidth = () => {
+  windowWidth.value = window.innerWidth;
+};
+
+onMounted(() => {
+  window.addEventListener('resize', updateWidth);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('resize', updateWidth);
+});
 </script>
 
 <style scoped>
